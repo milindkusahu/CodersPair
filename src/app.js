f0e2debe84b32514b6d2337f5e585b99a4f0d401
milindkusahu <milindsahu2000@gmail.com>
2025-04-14 16:46:19 +0530

    Add User schema

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
-const { connectDB } = require("../db/connect");
+const { connectDB } = require("./db/connect");
 const dotenv = require("dotenv");
+
+const { User } = require("./models/user.model");
 
 dotenv.config();
 
 const PORT = 3000;
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    username: "1",
+    firstName: "Milind",
+    lastName: "Sahu",
+    emailId: "milindsahu2000@gmail.com",
+    password: "milind123",
+  });
+
+  try {
+    await user.save();
+    res.send("User added successfully!");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
 
 const start = async () => {
   try {
---
+++ src/models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+    age: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  { timestamp: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
