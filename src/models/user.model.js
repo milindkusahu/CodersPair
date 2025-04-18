2ba083b785400f7702380782f7fb6b5874f22458
milindkusahu <milindsahu2000@gmail.com>
2025-04-19 02:31:32 +0530

    Schema validations

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,24 +5,44 @@ const userSchema = new mongoose.Schema(
     firstName: {
       type: String,
       required: true,
+      minLength: 4,
+      maxLength: 50,
     },
     lastName: {
       type: String,
     },
     emailId: {
       type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     password: {
       type: String,
+      required: true,
     },
     age: {
-      type: String,
+      type: Number,
+      min: 18,
     },
     gender: {
       type: String,
+      enum: ["male", "female", "others"],
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user!",
+    },
+    skills: {
+      type: [String],
     },
   },
-  { timestamp: true }
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
