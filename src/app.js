73c773fe988c5da5af4e513fb2fbd7f7daec729a
milindkusahu <milindsahu2000@gmail.com>
2025-04-19 03:38:24 +0530

    Exploring validator library

--- src/app.js
+++ src/app.js
@@ -73,12 +73,13 @@ app.patch("/user/:userId", async (req, res) => {
       throw new Error("Update not allowed");
     }
 
-    if (data?.skills.length > 10) {
+    if (data.skills && data.skills.length > 10) {
       throw new Error("Skills cannot be more than 10");
     }
 
     await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
+      runValidators: true,
     });
     res.send("User updated Successfully");
   } catch (err) {
