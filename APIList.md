8bdc3378d1ce4c7b64c1f9042f697e1339aa9d18
Milind Kumar Sahu <41789140+milindkusahu@users.noreply.github.com>
2025-04-29 17:44:05 +0530

    Add API endpoint to review connection requests with status validation

--- APIList.md
+++ APIList.md
@@ -16,8 +16,7 @@
 
 - POST /request/send/:status/:userId
 
-- POST /request/review/accepted/:requestId
-- POST /request/review/rejected/:requestId
+- POST /request/review/:status/:requestId
 
 ## userRouter
 
--- src/routes/request.routes.js
+++ src/routes/request.routes.js
@@ -57,4 +57,45 @@ router.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
   }
 });
 
+router.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const LoggedInUser = req.user;
+      const { status, requestId } = req.params;
+
+      // Validate the status
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({ message: "Status not allowed!" });
+      }
+
+      // Milind => Elon
+      // Is Elon LoggedIn => toUserId
+      // status = interested
+      // request Id should be valid
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: LoggedInUser._id,
+        status: "interested",
+      });
+
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: "Connection request not found" });
+      }
+
+      connectionRequest.status = status;
+
+      const data = await connectionRequest.save();
+
+      res.json({ message: `Connection request ${status}` });
+    } catch (err) {
+      res.status(400).send("ERROR : " + err.message);
+    }
+  }
+);
+
 module.exports = router;
