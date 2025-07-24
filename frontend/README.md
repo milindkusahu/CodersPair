0798c607cb2ec2100ef398d5c774fa5fc6e67b5c
Milind Kumar Sahu <41789140+milindkusahu@users.noreply.github.com>
2025-07-24 11:17:01 +0530

    Initialize frontend project with Vite, Tailwind CSS and daisyUI

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+Tech Stack: React, Tailwind CSS, daisyUI
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <br />
+      <div className="badge badge-soft badge-success">Success</div>
+    </>
+  );
+}
+
+export default App;
