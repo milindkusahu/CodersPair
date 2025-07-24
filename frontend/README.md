07da79d095a482f5aefa9a6f85405ea56b72e380
Milind Kumar Sahu <41789140+milindkusahu@users.noreply.github.com>
2025-07-24 12:15:31 +0530

    Set up project structure, routing, and global layout with header and footer

--- frontend/README.md
+++ frontend/README.md
@@ -1,1 +1,15 @@
-Tech Stack: React, Tailwind CSS, daisyUI
+Tech Stack: React, Tailwind CSS, daisyUI, React Router,
+
+Component Design
+~
+Body
+  NavBar
+  Route=/ => Feed
+  Route=/login => Login
+  Route=/connections => Connections
+  Route=/profile => Profile
+
+Routing Best Practice
+~
+-> Create BrowserRouter > Routes > Route=/ Body > RouteChildren
+-> Create an Outlet in the Body component
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/layout/Body";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <br />
-      <div className="badge badge-soft badge-success">Success</div>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
