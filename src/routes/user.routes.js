b434273977825bec59488dbf6c68675ac339fd8f
Milind Kumar Sahu <41789140+milindkusahu@users.noreply.github.com>
2025-07-24 10:18:44 +0530

    Add feed API with pagination

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { userAuth } = require("../middleware/auth.middleware");
 const ConnectionRequest = require("../models/connectionRequest.model");
+const { User } = require("../models/user.model");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
@@ -52,6 +53,52 @@ router.get("/user/connections", userAuth, async (req, res) => {
     });
   } catch (err) {
     res.status(400).send({ message: err.message });
+  }
+});
+
+router.get("/feed", userAuth, async (req, res) => {
+  try {
+    /**
+     * User should see all the user cards except
+     * -> His own card
+     * -> His connections
+     * -> Ignored people
+     * -> Already sent the connection request
+     */
+
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+
+    // Find all connection requests (sent + received)
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+
+    connectionRequest.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
